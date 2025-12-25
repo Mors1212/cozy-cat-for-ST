9b65619a4bd0f6b12ae092c94c724b81f432ed35
Mors1212 <demosnephel@gmail.com>
2025-12-25 17:26:01 +0100

    create mainfest,index.js,style.css

---
+++ index.js
@@ -0,0 +1,38 @@
+const extensionName = 'cozy-cat-for-ST';
+const authorConfig = {
+  name: 'Mors',
+  avatarUrl: '',
+};
+function loadSettings() {
+  $('.cozy-cat-settings').remove();
+
+  const settingsHtml = `
+        <div class="cozy-cat-settings">
+            <div class="inline-drawer">
+                <div class="inline-drawer-toggle inline-drawer-header">
+                    <b>Cozy Cat for ST</b>
+                    <div class="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
+                </div>
+
+                <div class="inline-drawer-content">
+                    <div class="styled_description_block">
+                        Extension by ${authorConfig.name} <br>
+                        <small>สถานะ: พร้อมทำงาน</small>
+                    </div>
+                    <hr>
+                    <div style="text-align: center; color: #aaa; padding: 20px;">
+                        (พื้นที่ว่าง)
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    `;
+
+  $('#extensions_settings').append(settingsHtml);
+}
+
+jQuery(async () => {
+  loadSettings();
+  console.log(`[${extensionName}] Panel Loaded.`);
+});
